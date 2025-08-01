571e8f2f0ff6413a67c8a7bf83e67231a05d8fdb
RazaALi0786 <razaali26296@gmail.com>
2025-08-02 03:39:28 +0530

    basic of get and post

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+from typing import Optional
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 app = FastAPI()
+
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
 
 @app.get("/")
 async def read_root():
@@ -8,3 +18,9 @@ async def read_root():
 @app.get("/posts")
 def get_posts():
     return {"data": "Here are your posts!"}
+
+@app.post("/createposts")
+def create_posts(post: Post):
+    print(post.rating)
+    print(post.dict())
+    return {"new_post": "success"} 
